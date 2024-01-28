151def83cf309c656407ddf1e30ce51def0b0552
timowin <timowinkler44@googlemail.com>
2024-01-28 22:02:00 +0100

    Update section component

--- src/components/SectionHeadline.tsx
+++ src/components/SectionHeadline.tsx
@@ -2,13 +2,18 @@ import { Typography } from "@material-tailwind/react";
 import React, { Component } from "react";
 
 type Props = {
-  title: String;
+  title: string;
+  textColor: string;
 };
 
 class SectionHeadline extends Component<Props> {
   render() {
     return (
-      <Typography placeholder={undefined} className="font-bold" variant="h2">
+      <Typography
+        placeholder={undefined}
+        className={`max-w-screen-lg mx-auto pt-20 pb-8 font-bold text-${this.props.textColor}`}
+        variant="h2"
+      >
         {this.props.title}
       </Typography>
     );
