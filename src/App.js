bf901c1bf9dbcc601364bc407a41786015988155
timowin <timowinkler44@googlemail.com>
2024-01-28 22:02:44 +0100

    Update App to mirror last commits

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import Hero from "./components/Hero";
 
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
 
-import { ExperienceType, Month } from "./Enums";
+import { ExperienceTypeEnum, MonthEnum, TechnologyTypeEnum } from "./Enums";
 import NavbarDefault from "./components/Navbar";
 import SectionHeadline from "./components/SectionHeadline";
 import ExperienceCard from "./components/ExperienceCard";
 import ProjectCard from "./components/ProjectCard";
 
 import timoimage from "./images/timo_winkler.jpeg";
-import sologo from "./images/sophisiticates-logo.png";
-import msglogo from "./images/msg-logo.png";
-import porschelogo from "./images/porsche-logo.png";
-import porscheengineeringlogo from "./images/porsche-engineering-logo.jpeg";
-import teleaktivlogo from "./images/teleaktiv-logo.jpeg";
-import jmulogo from "./images/jmu-logo.png";
-import thwslogo from "./images/thws-logo.png";
-import awscloudessentialsbadge from "./images/aws-cloud-essentials.png";
-import awscloudpractitioner from "./images/aws-cloud-practitioner.png";
+import sologo from "./images/companyLogos/sophisiticates-logo.png";
+import msglogo from "./images/companyLogos/msg-logo.png";
+import porschelogo from "./images/companyLogos/porsche-logo.png";
+import porscheengineeringlogo from "./images/companyLogos/porsche-engineering-logo.jpeg";
+import teleaktivlogo from "./images/companyLogos/teleaktiv-logo.jpeg";
+import jmulogo from "./images/companyLogos/jmu-logo.png";
+import thwslogo from "./images/companyLogos/thws-logo.png";
+import awscloudessentialsbadge from "./images/certificates/aws-cloud-essentials.png";
+import awscloudpractitioner from "./images/certificates/aws-cloud-practitioner.png";
 import { Typography } from "@material-tailwind/react";
 
 function App() {
@@ -42,17 +42,16 @@ function App() {
             <div className="flex flex-wrap gap-6 max-w-screen-lg mx-auto pb-20">
               <ExperienceCard
                 image={msglogo}
-                experience_type={ExperienceType.WORK}
-                start_month={Month.AUG}
+                experience_type={ExperienceTypeEnum.WORK}
+                start_month={MonthEnum.AUG}
                 start_year={2023}
-                end_month="Today"
                 title="IT Consultant"
                 type="Full time"
                 location="Frankfurt, Germany"
               />
               <ExperienceCard
                 image={awscloudpractitioner}
-                experience_type={ExperienceType.CERTIFICATE}
+                experience_type={ExperienceTypeEnum.CERTIFICATE}
                 date="August 24, 2023"
                 title="AWS Cloud Practitioner"
                 type="Certificate"
@@ -60,7 +59,7 @@ function App() {
               />
               <ExperienceCard
                 image={awscloudessentialsbadge}
-                experience_type={ExperienceType.CERTIFICATE}
+                experience_type={ExperienceTypeEnum.CERTIFICATE}
                 title="AWS Cloud Essentials"
                 date="August 22, 2023"
                 type="Certificate"
@@ -68,10 +67,10 @@ function App() {
               />
               <ExperienceCard
                 image={thwslogo}
-                experience_type={ExperienceType.EDUCATION}
-                start_month={Month.OCT}
+                experience_type={ExperienceTypeEnum.EDUCATION}
+                start_month={MonthEnum.OCT}
                 start_year={2019}
-                end_month={Month.MAR}
+                end_month={MonthEnum.MAR}
                 end_year={2023}
                 title="M.Sc. Information Systems"
                 type="Graduation"
@@ -79,10 +78,10 @@ function App() {
               />
               <ExperienceCard
                 image={porschelogo}
-                experience_type={ExperienceType.WORK}
-                start_month={Month.MAY}
+                experience_type={ExperienceTypeEnum.WORK}
+                start_month={MonthEnum.MAY}
                 start_year={2022}
-                end_month={Month.DEC}
+                end_month={MonthEnum.DEC}
                 end_year={2022}
                 title="Computer Vision Researcher"
                 type="Masterand"
@@ -90,10 +89,10 @@ function App() {
               />
               <ExperienceCard
                 image={porscheengineeringlogo}
-                experience_type={ExperienceType.WORK}
-                start_month={Month.OCT}
+                experience_type={ExperienceTypeEnum.WORK}
+                start_month={MonthEnum.OCT}
                 start_year={2020}
-                end_month={Month.SEP}
+                end_month={MonthEnum.SEP}
                 end_year={2021}
                 title="Software Quality- and Process Manager"
                 type="Trainee"
@@ -101,10 +100,10 @@ function App() {
               />
               <ExperienceCard
                 image={sologo}
-                experience_type={ExperienceType.WORK}
-                start_month={Month.SEP}
+                experience_type={ExperienceTypeEnum.WORK}
+                start_month={MonthEnum.SEP}
                 start_year={2019}
-                end_month={Month.SEP}
+                end_month={MonthEnum.SEP}
                 end_year={2021}
                 title="Full Stack Developer"
                 type="Working Student"
@@ -112,10 +111,10 @@ function App() {
               />
               <ExperienceCard
                 image={teleaktivlogo}
-                experience_type={ExperienceType.WORK}
-                start_month={Month.DEC}
+                experience_type={ExperienceTypeEnum.WORK}
+                start_month={MonthEnum.DEC}
                 start_year={2017}
-                end_month={Month.AUG}
+                end_month={MonthEnum.AUG}
                 end_year={2019}
                 title="Data Quality Analyst"
                 type="Working Student"
@@ -123,10 +122,10 @@ function App() {
               />
               <ExperienceCard
                 image={teleaktivlogo}
-                experience_type={ExperienceType.WORK}
-                start_month={Month.NOV}
+                experience_type={ExperienceTypeEnum.WORK}
+                start_month={MonthEnum.NOV}
                 start_year={2018}
-                end_month={Month.MAR}
+                end_month={MonthEnum.MAR}
                 end_year={2019}
                 title="Data Quality Researcher"
                 type="Bachelorand"
@@ -134,10 +133,10 @@ function App() {
               />
               <ExperienceCard
                 image={jmulogo}
-                experience_type={ExperienceType.EDUCATION}
-                start_month={Month.OCT}
+                experience_type={ExperienceTypeEnum.EDUCATION}
+                start_month={MonthEnum.OCT}
                 start_year={2014}
-                end_month={Month.MAR}
+                end_month={MonthEnum.MAR}
                 end_year={2019}
                 title="B.Sc. Information Systems"
                 type="Graduation"
@@ -157,94 +156,106 @@ function App() {
             <div className="max-w-screen-lg mx-auto flex flex-col gap-6">
               <ProjectCard
                 title="GreenFactory"
-                start_month="Jan"
+                start_month={MonthEnum.JAN}
                 start_year={2024}
                 end_month="Today"
-                technologies={["Github", "Python", "Blabla"]}
+                technologies={[
+                  ["Github", TechnologyTypeEnum.TOOL],
+                  ["Python", TechnologyTypeEnum.LANGUAGE],
+                ]}
               ></ProjectCard>
               <ProjectCard
                 title="Validation as a Service"
-                start_month="Sep"
+                start_month={MonthEnum.SEP}
                 start_year={2023}
-                end_month="Jan"
+                end_month={MonthEnum.JAN}
                 end_year={2024}
                 technologies={[
-                  "Python",
-                  "Flask",
-                  "Postgresql",
-                  "Gitea",
-                  "Docker",
+                  ["Python", TechnologyTypeEnum.LANGUAGE],
+                  ["Flask", TechnologyTypeEnum.FRAMEWORK],
+                  ["Postgresql", TechnologyTypeEnum.DATABASE],
+                  ["Gitea", TechnologyTypeEnum.TOOL],
+                  ["Docker", TechnologyTypeEnum.TOOL],
                 ]}
               ></ProjectCard>
               <ProjectCard
                 title="Solar Consultant Tool"
-                start_month="Jun"
+                start_month={MonthEnum.JUN}
                 start_year={2023}
                 end_month="Today"
                 technologies={[
-                  "React",
-                  "TypeScript",
-                  "JavaScript",
-                  "Tailwind",
-                  "CSS",
-                  "GitHub",
+                  ["React", TechnologyTypeEnum.FRAMEWORK],
+                  ["TypeScript", TechnologyTypeEnum.LANGUAGE],
+                  ["JavaScript", TechnologyTypeEnum.LANGUAGE],
+                  ["Tailwind", TechnologyTypeEnum.FRAMEWORK],
+                  ["CSS", TechnologyTypeEnum.LANGUAGE],
+                  ["GitHub", TechnologyTypeEnum.TOOL],
                 ]}
               ></ProjectCard>
               <ProjectCard
                 title="Masterthesis"
-                start_month="Jul"
+                start_month={MonthEnum.JUL}
                 start_year={2022}
-                end_month="Jan"
+                end_month={MonthEnum.JAN}
                 end_year={2023}
                 technologies={[
-                  "Python",
-                  "TeX",
-                  "Object Recognition",
-                  "Statistic",
-                  "Github",
+                  ["Python", TechnologyTypeEnum.LANGUAGE],
+                  ["TeX", TechnologyTypeEnum.LANGUAGE],
+                  ["Object Recognition", TechnologyTypeEnum.MISC],
+                  ["Statistic", TechnologyTypeEnum.MISC],
+                  ["Github", TechnologyTypeEnum.TOOL],
                 ]}
               ></ProjectCard>
               <ProjectCard
                 title="Autonomous Driving Challenge"
-                start_month="Okt"
+                start_month={MonthEnum.OCT}
                 start_year={2020}
-                end_month="Feb"
+                end_month={MonthEnum.FEB}
                 end_year={2021}
                 technologies={[
-                  "Python",
-                  "Nvidia Jetbot",
-                  "Pytorch",
-                  "Jupyter Notebook",
-                  "GitHub",
+                  ["Python", TechnologyTypeEnum.LANGUAGE],
+                  ["Nvidia Jetbot", TechnologyTypeEnum.MISC],
+                  ["Pytorch", TechnologyTypeEnum.FRAMEWORK],
+                  ["Jupyter Notebook", TechnologyTypeEnum.TOOL],
+                  ["GitHub", TechnologyTypeEnum.TOOL],
                 ]}
               ></ProjectCard>
               <ProjectCard
                 title="Brokerware"
-                start_month="Okt"
+                start_month={MonthEnum.OCT}
                 start_year={2020}
-                end_month="Sep"
+                end_month={MonthEnum.SEP}
                 end_year={2021}
                 technologies={[
-                  "Ruby on Rails",
-                  "Postgresql",
-                  "Docker",
-                  "GitLab",
+                  ["Ruby on Rails", TechnologyTypeEnum.LANGUAGE],
+                  ["Postgresql", TechnologyTypeEnum.DATABASE],
+                  ["Docker", TechnologyTypeEnum.TOOL],
+                  ["GitLab", TechnologyTypeEnum.TOOL],
                 ]}
               ></ProjectCard>
               <ProjectCard
                 title="SPT-Generator"
-                start_month="Jul"
+                start_month={MonthEnum.JUL}
                 start_year={2019}
-                end_month="Mar"
+                end_month={MonthEnum.MAR}
                 end_year={2021}
-                technologies={["Python", "PyQt", "PDF Generation"]}
+                technologies={[
+                  ["Python", TechnologyTypeEnum.LANGUAGE],
+                  ["PyQt", TechnologyTypeEnum.FRAMEWORK],
+                  ["PDF Generation", TechnologyTypeEnum.MISC],
+                ]}
               ></ProjectCard>
               <ProjectCard
                 title="Bachelorthesis"
-                start_month="Nov"
+                start_month={MonthEnum.NOV}
                 start_year={2018}
-                end_month="Mar"
+                end_month={MonthEnum.MAR}
                 end_year={2019}
+                technologies={[
+                  ["Python", TechnologyTypeEnum.LANGUAGE],
+                  ["MongoDB", TechnologyTypeEnum.DATABASE],
+                  ["PDF Generation", TechnologyTypeEnum.MISC],
+                ]}
               ></ProjectCard>
             </div>
           </section>
