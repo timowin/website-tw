84a4dfa0f68682650b5528d0fa2f4dcbf74d935f
timowin <timowinkler44@googlemail.com>
2024-01-29 12:27:50 +0100

    move experience data to experience.json file

--- src/components/ImageWithLocation.tsx
+++ src/components/ImageWithLocation.tsx
@@ -1,20 +1,20 @@
 import { MapPinIcon } from "@heroicons/react/24/outline";
 import React, { Component } from "react";
+import imageObjects from "../images/imageLoader";
 
 type Props = {
-  image: string;
+  image_name: string;
   location: string;
 };
 
 export default class ImageWithLocation extends Component<Props> {
-  state = {};
-
   render() {
+    const imageObjectName = this.props.image_name as keyof typeof imageObjects;
     return (
       <div className="flex flex-col gap-8 justify-end">
         <img
-          src={this.props.image}
-          alt="brand-logo"
+          src={imageObjects[imageObjectName]}
+          alt="logo"
           className="max-h-32 object-contain mx-auto"
         />
         <span className="content-center mx-auto italic text-gray-600 text-sm group-hover:text-secondary flex flex-row gap-2">
