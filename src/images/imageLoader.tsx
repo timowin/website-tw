7dad03d9d09b5eba0884dce5f61dba756b05bc9a
timowin <timowinkler44@googlemail.com>
2024-01-29 12:58:43 +0100

    adds new certificates

--- src/images/imageLoader.tsx
+++ src/images/imageLoader.tsx
@@ -8,5 +8,7 @@ const imageObjects = {
   teleAktivLogo: require("./companyLogos/teleaktiv-logo.jpeg"),
   awsCloudEssentialsCertificate: require("./certificates/aws-cloud-essentials.png"),
   awsCloudPractitionerCertificate: require("./certificates/aws-cloud-practitioner.png"),
+  bankingKnowledgeCertificate: require("./certificates/bankfachwissen-kompakt.png"),
+  interculturalCertificate: require("./certificates/intercultural-competence.png"),
 };
 export default imageObjects;
