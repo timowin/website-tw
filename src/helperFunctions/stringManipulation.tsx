8ee4cd65bc563dfa09888bcd9f31cbc61eb26ee4
timowin <timowinkler44@googlemail.com>
2024-01-29 12:28:53 +0100

    handle undefined and null values in string manipulation method

--- src/helperFunctions/stringManipulation.tsx
+++ src/helperFunctions/stringManipulation.tsx
@@ -1,4 +1,7 @@
-function capitalizeToLowercase(str: string): string {
+function capitalizeToLowercase(str: string): string | undefined {
+  if (!str) {
+    return undefined;
+  }
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 export { capitalizeToLowercase };
