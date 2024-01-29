893d357a281d58744ebb75f31b881d1900e6974a
timowin <timowinkler44@googlemail.com>
2024-01-29 12:28:11 +0100

    Introduce imageloader

---
+++ src/images/imageLoader.tsx
@@ -0,0 +1,12 @@
+const imageObjects = {
+  msgLogo: require("./companyLogos/msg-logo.png"),
+  porscheLogo: require("./companyLogos/porsche-logo.png"),
+  porscheEngineeringLogo: require("./companyLogos/porsche-engineering-logo.jpeg"),
+  sophisticatesLogo: require("./companyLogos/sophisiticates-logo.png"),
+  jmuLogo: require("./companyLogos/jmu-logo.png"),
+  thwsLogo: require("./companyLogos/thws-logo.png"),
+  teleAktivLogo: require("./companyLogos/teleaktiv-logo.jpeg"),
+  awsCloudEssentialsCertificate: require("./certificates/aws-cloud-essentials.png"),
+  awsCloudPractitionerCertificate: require("./certificates/aws-cloud-practitioner.png"),
+};
+export default imageObjects;
