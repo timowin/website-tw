2da9c3e98804f901b422b25dfa3cf2c18b4d8ea3
timowin <timowinkler44@googlemail.com>
2024-02-04 18:12:30 +0100

    adds fix height to certification section

--- src/pages/landing/CertificationSection.tsx
+++ src/pages/landing/CertificationSection.tsx
@@ -4,17 +4,19 @@ import certifications from "../../data/certifications.json";
 
 const CertificationSection = () => {
   return (
-    <section className="snap-start bg-primary py-20 rounded-0 lg:rounded-lg lg:mx-6">
-      <SectionHeadline title="Certifications" textColor="white" />
-      <div className="flex flex-wrap gap-6 max-w-screen-lg mx-auto px-6 lg:py-0">
-        {certifications.map((certificate) => (
-          <CertificateCard
-            image_name={certificate.image_name}
-            title={certificate.title}
-            location={certificate.location}
-            date_of_completion={certificate.date}
-          />
-        ))}
+    <section className="snap-start h-screen">
+      <div className="bg-primary py-20 my-6 h-[calc(100vh-3rem)] rounded-0 lg:rounded-lg lg:mx-6">
+        <SectionHeadline title="Certifications" textColor="white" />
+        <div className="flex flex-wrap gap-6 max-w-screen-lg mx-auto px-6 lg:py-0">
+          {certifications.map((certificate) => (
+            <CertificateCard
+              image_name={certificate.image_name}
+              title={certificate.title}
+              location={certificate.location}
+              date_of_completion={certificate.date}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
